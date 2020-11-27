07da25a92d419c37e0b9a2cd29600639fc09082c
sammoth <sammoth.th@gmail.com>
2020-11-27 12:27:55 +0000

    use rusqlite

--- migrations/2020-11-24-162158_create_items/down.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE metadbs;
-DROP TABLE tags;
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,19 @@
-#[macro_use]
-extern crate diesel;
-#[macro_use]
-extern crate diesel_migrations;
-
 extern crate ape;
+extern crate evmap;
 extern crate ffav as ffmpeg;
 extern crate flume;
 extern crate ignore;
 extern crate lazy_static;
 extern crate mime_guess;
 extern crate rayon;
+extern crate rusqlite;
 extern crate structopt;
 extern crate tree_magic;
 
-pub mod models;
-pub mod schema;
-
-use diesel::prelude::*;
 use rayon::prelude::*;
 use structopt::StructOpt;
 
-use self::models::*;
-
-type SqlitePool = diesel::r2d2::Pool<diesel::r2d2::ConnectionManager<SqliteConnection>>;
-
-lazy_static::lazy_static! {
-    static ref POOL: SqlitePool = {
-    let dirs = directories_next::ProjectDirs::from("", "", "momo").unwrap();
-    let db_path = dirs.cache_dir().to_path_buf();
-    let mut db_file = db_path.to_path_buf();
-    std::fs::create_dir_all(db_path).unwrap();
-    db_file.push("library.db");
-    let db = db_file.to_str().unwrap();
-
-    let manager = diesel::r2d2::ConnectionManager::<SqliteConnection>::new(db);
-    diesel::r2d2::Pool::builder()
-        .max_size(1)
-        .build(manager)
-        .unwrap()
-    };
-}
+use rusqlite::{params, Connection, Result};
 
 #[derive(StructOpt)]
 #[structopt(about = "media scanner")]
@@ -48,36 +22,92 @@ enum Command {
         #[structopt(parse(from_os_str))]
         path: std::path::PathBuf,
     },
-    Rescan {
-        #[structopt(parse(from_os_str))]
-        path: std::path::PathBuf,
-    },
-    // Search {
-    //     #[structopt(short)]
-    //     field: String,
-    //     #[structopt(short)]
-    //     query: String,
+    // Rescan {
+    //     #[structopt(parse(from_os_str))]
+    //     path: std::path::PathBuf,
     // },
+    Search {
+        #[structopt(short)]
+        field: String,
+        #[structopt(short)]
+        query: String,
+    },
 }
 
-diesel_migrations::embed_migrations!();
+lazy_static::lazy_static! {
+    static ref DB: std::path::PathBuf = {
+    let dirs = directories_next::ProjectDirs::from("", "", "momo").unwrap();
+    let db_path = dirs.cache_dir().to_path_buf();
+    let mut db_file = db_path.to_path_buf();
+    std::fs::create_dir_all(db_path).unwrap();
+    db_file.push("library.db");
+    db_file
+    };
+}
 
-fn main() {
-    {
-        let connection = POOL.get().unwrap();
-        embedded_migrations::run(&connection).unwrap();
-    }
-
+fn main() -> Result<()> {
     let args = Command::from_args();
+
+    {
+        let connection = Connection::open_with_flags(
+            DB.as_path(),
+            rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE | rusqlite::OpenFlags::SQLITE_OPEN_CREATE,
+        )?;
+        connection.execute("PRAGMA foreign_keys = ON;", params![])?;
+        connection.execute(
+            "CREATE TABLE IF NOT EXISTS metadb (
+                      id              INTEGER PRIMARY KEY,
+                      location        TEXT NOT NULL,
+                      subsong         INTEGER
+                      )",
+            params![],
+        )?;
+        connection.execute(
+            "CREATE UNIQUE INDEX IF NOT EXISTS track ON metadb (location, subsong)",
+            params![],
+        )?;
+        connection.execute(
+            "CREATE TABLE IF NOT EXISTS tag(
+                metadb              INTEGER NOT NULL,
+                key                 TEXT NOT NULL, 
+                value               TEXT NOT NULL,
+                FOREIGN KEY(metadb) REFERENCES metadb(id),
+                PRIMARY KEY (metadb, key)
+              );",
+            params![],
+        )?;
+    }
 
     match args {
         Command::Scan { path } => {
             scan_library(path);
         }
-        Command::Rescan { path } => {
-            scan_library(path);
+        // Command::Rescan { path } => {
+        //     scan_library(path);
+        // }
+        Command::Search { field, mut query } => {
+            let connection = Connection::open_with_flags(
+                DB.as_path(),
+                rusqlite::OpenFlags::SQLITE_OPEN_READ_ONLY,
+            )?;
+
+            let mut stmt = connection.prepare(
+                "SELECT location, value FROM metadb m JOIN tag t on t.metadb = m.id WHERE key = ?1 and value LIKE ?2",
+            )?;
+
+            query.push('%');
+            query.insert(0, '%');
+            let mut rows = stmt.query(params![field, query])?;
+
+            while let Some(row) = rows.next()? {
+                let location: String = row.get(0)?;
+                let value: String = row.get(1)?;
+                println!("{} : {}", location, value);
+            }
         }
     }
+
+    Ok(())
 }
 
 fn scan_library(path: std::path::PathBuf) {
@@ -98,14 +128,25 @@ fn scan_library(path: std::path::PathBuf) {
         .build_parallel();
 
     let rayon_consumer = std::thread::spawn(move || {
-        rx.into_iter().par_bridge().for_each(|entry| {
-            scan_file(entry);
-            println!(
-                "{:?} / {:?}",
-                COUNT_COMPLETE.fetch_add(1, std::sync::atomic::Ordering::SeqCst) + 1,
-                COUNT_TOTAL,
-            );
-        });
+        rx.into_iter()
+            .par_bridge()
+            .for_each(|entry| match scan_file(entry) {
+                Ok(_) => {
+                    println!(
+                        "{:?} / {:?}",
+                        COUNT_COMPLETE.fetch_add(1, std::sync::atomic::Ordering::SeqCst) + 1,
+                        COUNT_TOTAL,
+                    );
+                }
+                Err(err) => {
+                    println!(
+                        "{:?} / {:?} (Error {:?})",
+                        COUNT_COMPLETE.fetch_add(1, std::sync::atomic::Ordering::SeqCst) + 1,
+                        COUNT_TOTAL,
+                        err,
+                    );
+                }
+            });
     });
 
     walker.run(|| {
@@ -139,35 +180,31 @@ fn scan_library(path: std::path::PathBuf) {
     rayon_consumer.join().unwrap();
 }
 
-fn scan_file(entry: ignore::DirEntry) {
+fn scan_file(entry: ignore::DirEntry) -> Result<()> {
+    let path = entry.path().to_str().unwrap();
+
     let mime = mime_guess::from_path(entry.path())
         .first()
         .map(|mime| mime.essence_str().to_string())
         .unwrap_or(tree_magic::from_filepath(entry.path()));
 
     if mime.starts_with("audio") || mime.starts_with("video") {
-        let connection = POOL.get().unwrap();
-
-        use schema::metadbs;
-        use schema::tags;
-
-        let new_metadb = NewMetadb {
-            location: entry.path().to_str().unwrap(),
-            subsong: &0,
-            mimetype: &mime,
-        };
-
-        diesel::insert_into(metadbs::table)
-            .values(&new_metadb)
-            .execute(&connection)
-            .expect("error inserting");
-
-        use schema::metadbs::dsl::*;
-        let new_id: i32 = metadbs
-            .order(id.desc())
-            .select(id)
-            .first(&connection)
-            .expect("no id from insert");
+        let connection = Connection::open_with_flags(
+            DB.as_path(),
+            rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE | rusqlite::OpenFlags::SQLITE_OPEN_CREATE,
+        )?;
+        connection.execute(
+            "INSERT INTO metadb (
+                location,
+                subsong
+            ) VALUES (?1, ?2)",
+            params![path, 0],
+        )?;
+        let metadb_id: i32 = connection.query_row(
+            "SELECT id FROM metadb WHERE location = ?1 and subsong = ?2",
+            params![path, 0],
+            |row| row.get(0),
+        )?;
 
         match ffmpeg::format::input(&entry.path()) {
             Ok(context) => {
@@ -175,15 +212,14 @@ fn scan_file(entry: ignore::DirEntry) {
                     || context.streams().best(ffmpeg::media::Type::Audio).is_some()
                 {
                     for (k, v) in context.metadata().iter() {
-                        let new_tag = NewTag {
-                            id: &new_id,
-                            key: &k.to_lowercase(),
-                            value: v,
-                        };
-
-                        diesel::insert_into(tags::table)
-                            .values(&new_tag)
-                            .execute(&connection);
+                        connection.execute(
+                            "INSERT INTO tag (
+                                metadb,
+                                key,
+                                value
+                            ) VALUES (?1, ?2, ?3)",
+                            params![metadb_id, k.to_lowercase(), v],
+                        )?;
                     }
                 }
             }
@@ -200,17 +236,18 @@ fn scan_file(entry: ignore::DirEntry) {
         if let Ok(tag) = ape::read(entry.path()) {
             for item in tag {
                 if let ape::ItemValue::Text(s) = item.value {
-                    let new_tag = NewTag {
-                        id: &new_id,
-                        key: &item.key.to_lowercase(),
-                        value: &s,
-                    };
-
-                    diesel::insert_into(tags::table)
-                        .values(&new_tag)
-                        .execute(&connection);
+                    connection.execute(
+                        "INSERT INTO tag (
+                                metadb,
+                                key,
+                                value
+                            ) VALUES (?1, ?2, ?3)",
+                        params![metadb_id, item.key.to_lowercase(), s],
+                    )?;
                 };
             }
         }
     }
+
+    Ok(())
 }
